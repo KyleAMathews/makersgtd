d61a4dfbc0a847ef276d5cec281c7ba63c88c1ca
Kyle Mathews <mathews.kyle@gmail.com>
2012-02-14 11:10:25 -0800

    Made path explicit so will work as post-commit hook

--- sync.sh
+++ sync.sh
@@ -1,4 +1,4 @@
 rsync * -rzhv --stats --delete eduglu:/var/nodejs/SimpleGTD --exclude node_modules --exclude dump --exclude brunch/src -C
 ssh eduglu 'mongodump --db simpleGTD --out /var/nodejs/SimpleGTD/dump/'
-scp eduglu:/var/nodejs/SimpleGTD/dump/simpleGTD/* ./dump/simpleGTD/
+scp eduglu:/var/nodejs/SimpleGTD/dump/simpleGTD/* /var/www/makersgtd/dump/simpleGTD/
 mongorestore --drop --db simpleGTD /var/www/makersgtd/dump/simpleGTD/
