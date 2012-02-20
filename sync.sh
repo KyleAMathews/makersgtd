2ef2a366a10e03f6b44f42b6380ebc8ead6f3740
Kyle Mathews <mathews.kyle@gmail.com>
2012-02-20 15:12:01 -0800

    Change deploy

--- sync.sh
+++ sync.sh
@@ -1,4 +1,5 @@
-rsync /var/www/makersgtd/* -rzhv --stats --delete eduglu:/var/nodejs/SimpleGTD --exclude node_modules --exclude dump --exclude brunch/src -C
+#rsync /var/www/makersgtd/* -rzhv --stats --delete eduglu:/var/nodejs/SimpleGTD --exclude node_modules --exclude dump --exclude brunch/src -C
+ssh -A eduglu 'sh /var/nodejs/makersgtd/deploy'
 ssh eduglu 'mongodump --db simpleGTD --out /var/nodejs/SimpleGTD/dump/'
 scp eduglu:/var/nodejs/SimpleGTD/dump/simpleGTD/* /var/www/makersgtd/dump/simpleGTD/
 mongorestore --drop --db simpleGTD /var/www/makersgtd/dump/simpleGTD/
