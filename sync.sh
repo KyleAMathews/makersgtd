3fa3400e3b9ec5518439fda0327792653cb4e334
Kyle Mathews <mathews.kyle@gmail.com>
2012-04-09 12:57:06 -0700

    Updated sync code for new server

--- sync.sh
+++ sync.sh
@@ -1,5 +1,5 @@
 #rsync /var/www/makersgtd/* -rzhv --stats --delete eduglu:/var/nodejs/SimpleGTD --exclude node_modules --exclude dump --exclude brunch/src -C
-ssh -A eduglu 'sh /var/nodejs/makersgtd/deploy'
-ssh eduglu 'mongodump --db simpleGTD --out /var/nodejs/SimpleGTD/dump/'
-scp eduglu:/var/nodejs/SimpleGTD/dump/simpleGTD/* /var/www/makersgtd/dump/simpleGTD/
+ssh -A makersgtd 'sh /opt/makersgtd/deploy.sh'
+ssh makersgtd 'mongodump --db simpleGTD --out /opt/makersgtd/SimpleGTD/dump/'
+scp makersgtd:/opt/makersgtd/SimpleGTD/dump/simpleGTD/* /var/www/makersgtd/dump/simpleGTD/
 mongorestore --drop --db simpleGTD /var/www/makersgtd/dump/simpleGTD/
